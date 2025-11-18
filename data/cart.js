cf5869970f26e8c99e5442510326b129f55f0af6
ElocCode <colephilips22@hotmail.com>
2025-11-18 17:08:22 -0500

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [];
+export let cart = [];
 
 export function addToCart(productId) {
     let matchingItem;
@@ -16,3 +16,14 @@ export function addToCart(productId) {
         });
     };
 };
+
+function removeFromCart (productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if(cartItem.productId != productId) {
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart;
+}
