0d2a086cf9c7575458d7d899037fc983df2eccca
ElocCode <colephilips22@hotmail.com>
2025-12-15 18:01:00 -0500

    Create automated tests for addToCart. Also figured out bug with jasmine where it would properly mock localStorage if using firefox

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,11 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
     cart = [{
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 2,
@@ -11,51 +16,58 @@ if (!cart) {
       deliveryOptionId: '2'
     }];
   }
+}
 
 function saveToStorage() {
-    localStorage.setItem('cart', JSON.stringify(cart));
-};
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
-    let matchingItem;
-    cart.forEach((cartItem) => {
-        if (productId === cartItem.productId){
-            matchingItem = cartItem;
-        }
-    });
-    if (matchingItem){
-        matchingItem.quantity++;
-    } else{
-        cart.push({
-            productId: productId,
-            quantity:1,
-            deliveryOptionId: '1'
-        });
-    };
-    saveToStorage();
-};
+  let matchingItem;
 
-export function removeFromCart (productId) {
-    const newCart = [];
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
 
-    cart.forEach((cartItem) => {
-        if(cartItem.productId != productId) {
-            newCart.push(cartItem);
-        }
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
     });
-    cart = newCart;
-    saveToStorage();
+  }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
 }
 
 export function updateDeliveryOption(productId, deliveryOptionId) {
-    let matchingItem;
-    cart.forEach((cartItem) => {
-        if (productId === cartItem.productId){
-            matchingItem = cartItem;
-        }
-    });
+  let matchingItem;
 
-    matchingItem.deliveryOptionId = deliveryOptionId;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
 
-    saveToStorage();
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
 }
